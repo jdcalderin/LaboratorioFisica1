cb4a6085f97afd238b95badb743f697e8cfaf092
CarlosArturo <cartpower15@gmail.com>
2017-11-12 17:39:38 -0500

    velocidad

--- app/src/main/java/com/tesis/orca/laboratoriofisica1/Activity_Calc_Teorica.java
+++ app/src/main/java/com/tesis/orca/laboratoriofisica1/Activity_Calc_Teorica.java
@@ -1,13 +1,17 @@
 package com.tesis.orca.laboratoriofisica1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class Activity_Calc_Teorica extends AppCompatActivity {
+
+    public static final String strValorRadio = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,14 @@ public class Activity_Calc_Teorica extends AppCompatActivity {
             }
         });
     }
+
+    public void Ircalcular(View view) {
+        Intent intent = new Intent(this, Activity_result_teorico.class);
+        EditText etRadio = (EditText) findViewById(R.id.etRadio);
+        String strRadio = etRadio.getText().toString();
+        intent.putExtra(strValorRadio, strRadio);
+        startActivity(intent);
+    }
+
 
 }
